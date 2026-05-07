7d516e8d41d18ff69d26e2f46a1f712940baf752
Khirru Clyde Gavilo <khirruclyde.gavilo@stratpoint.com>
2026-05-07 11:48:03 +0800

    chore: add project scaffolding, Express app, and Prisma client

---
+++ module-10-capstone/src/routes/notes.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const prisma = require("../db");
+
+const router = express.Router();
+
+// Get all notes, sorted by lastest, filtered by a tag, and can be paginated
+app.get("/tasks", async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+// Get a specific note by id
+app.get("/tasks/:id", async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+// Create a new note
+app.post("/tasks", async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+// Update a specific note by id
+app.put("/tasks/:id", async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+// Delete a specific note by id
+app.delete("/tasks/:id", async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+module.exports = router;
